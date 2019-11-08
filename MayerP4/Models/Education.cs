50ac5dee675d9030af27ff8f446126ae29588b2d
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 12:20:22 -0700

    Add Education model

---
+++ MayerP4/Models/Education.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayerP4.Models
+{
+    public class Education
+    {
+        public int EducationID { get; set; }
+        public string Degree { get; set; }
+        public DateTime End { get; set; }
+        public DateTime Start { get; set; }
+        public string School { get; set; }
+    }
+}
