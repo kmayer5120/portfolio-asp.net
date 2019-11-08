670fb8934c2f0af657567776141700cfedbab542
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 12:40:42 -0700

    Add ResumeViewModel

---
+++ MayerP4/Models/ResumeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayerP4.Models
+{
+    public class ResumeViewModel
+    {
+        public string Intro { get; set; }
+        public List<Skill> Skils { get; set; }
+        public List<Experience> Experiences { get; set; }
+        public List<Education> Educations { get; set; }
+        public string Conclusion { get; set; }
+    }
+}
