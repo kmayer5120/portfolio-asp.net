13bdfec3e55e6edd71b2e94f4bd1fd6d68b6eac4
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 13:23:43 -0700

    Added Blog model

---
+++ MayerP4/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayerP4.Models
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public string Post { get; set; }
+        public string Description { get; set; }
+
+    }
+}
