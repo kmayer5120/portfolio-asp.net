f3b8466687d77302477dfcd01f521bd409e19718
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 12:24:12 -0700

    Add Skill model

---
+++ MayerP4/Models/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayerP4.Models
+{
+    public class Skill
+    {
+        public int SkillID { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+}
