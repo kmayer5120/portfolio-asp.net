1717955dc487a9759dce70d554ebe9d3555addef
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 13:15:31 -0700

    Basic Resume controller added

---
+++ MayerP4/Controllers/ResumeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MayerP4.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MayerP4.Data;
+
+namespace MayerP4.Controllers
+{
+    public class ResumeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResumeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resume
+        public ActionResult Index()
+        {
+            var vm = new ResumeViewModel();
+            vm.Intro = "some text for an intro"; //can be hardcoded
+            vm.Skills = _context.Skills.ToList();
+            vm.Experiences = _context.Experiences.ToList();
+            vm.Educations = _context.Educations.ToList();
+
+            return View(vm);
+        }
+
+        // GET: Resume/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Resume/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resume/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resume/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Resume/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resume/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Resume/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
