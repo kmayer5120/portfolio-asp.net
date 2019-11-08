33f10c7676288050c1ecff5e84b98f94cba994e1
kmayer5120 <kmayer1@cnm.edu>
2019-11-08 12:44:26 -0700

    Added ctor for ResumeViewModel

--- MayerP4/Models/ResumeViewModel.cs
+++ MayerP4/Models/ResumeViewModel.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MayerP4.Data;
 
 namespace MayerP4.Models
 {
     public class ResumeViewModel
     {
+        private ApplicationDbContext db;
+
         public string Intro { get; set; }
         public List<Skill> Skils { get; set; }
         public List<Experience> Experiences { get; set; }
         public List<Education> Educations { get; set; }
         public string Conclusion { get; set; }
+
+        public ResumeViewModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
     }
 }
