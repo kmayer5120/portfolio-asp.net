3044b0b22b56b63c86e5104a06eaaa4df5f91b74
Kyle Mayer <kmayer1@cnm.edu>
2019-11-18 16:29:52 -0700

    Added controllers for Resume items

---
+++ MayerP4/Controllers/EducationsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MayerP4.Data;
+using MayerP4.Models;
+
+namespace MayerP4.Controllers
+{
+    public class EducationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EducationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Educations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Educations.ToListAsync());
+        }
+
+        // GET: Educations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations
+                .FirstOrDefaultAsync(m => m.EducationID == id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+
+            return View(education);
+        }
+
+        // GET: Educations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Educations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EducationID,Degree,End,Start,School")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(education);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(education);
+        }
+
+        // GET: Educations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations.FindAsync(id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+            return View(education);
+        }
+
+        // POST: Educations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EducationID,Degree,End,Start,School")] Education education)
+        {
+            if (id != education.EducationID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(education);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EducationExists(education.EducationID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(education);
+        }
+
+        // GET: Educations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var education = await _context.Educations
+                .FirstOrDefaultAsync(m => m.EducationID == id);
+            if (education == null)
+            {
+                return NotFound();
+            }
+
+            return View(education);
+        }
+
+        // POST: Educations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var education = await _context.Educations.FindAsync(id);
+            _context.Educations.Remove(education);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EducationExists(int id)
+        {
+            return _context.Educations.Any(e => e.EducationID == id);
+        }
+    }
+}
---
+++ MayerP4/Controllers/SkillsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MayerP4.Data;
+using MayerP4.Models;
+
+namespace MayerP4.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SkillsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Skills
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Skills.ToListAsync());
+        }
+
+        // GET: Skills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillID == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // GET: Skills/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SkillID,Description,Title")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(skill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skill);
+        }
+
+        // GET: Skills/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SkillID,Description,Title")] Skill skill)
+        {
+            if (id != skill.SkillID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(skill);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillExists(skill.SkillID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skill);
+        }
+
+        // GET: Skills/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillID == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var skill = await _context.Skills.FindAsync(id);
+            _context.Skills.Remove(skill);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SkillExists(int id)
+        {
+            return _context.Skills.Any(e => e.SkillID == id);
+        }
+    }
+}
