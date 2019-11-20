def1493f4a4a6b15c4dbd6387b5c84201e4c6ba2
kmayer5120 <kmayer1@cnm.edu>
2019-11-20 13:52:59 -0700

    Added Sent view

--- MayerP4/Models/Education.cs
+++ MayerP4/Models/Education.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,15 @@ namespace MayerP4.Models
     public class Education
     {
         public int EducationID { get; set; }
+
         public string Degree { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime End { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Start { get; set; }
+        
         public string School { get; set; }
     }
 }
