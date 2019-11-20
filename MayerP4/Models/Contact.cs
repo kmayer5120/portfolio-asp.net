121155626beaa92b5c04f0b6049088ec2cbbb3a6
kmayer5120 <kmayer1@cnm.edu>
2019-11-20 13:34:37 -0700

    Added display names for properties

--- MayerP4/Models/Contact.cs
+++ MayerP4/Models/Contact.cs
@@ -13,14 +13,17 @@ namespace MayerP4.Models
 
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [Required]
         [StringLength(20, MinimumLength = 5)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required]
         [StringLength(20, MinimumLength = 5)]
         public string LastName { get; set; }
 
+        [Display(Name = "Email Address")]
         [Required]
         [EmailAddress]
         public string Email { get; set; }
