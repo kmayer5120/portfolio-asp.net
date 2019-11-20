559c0575c4941bf5cedae8a6abec5baa6b18a891
kmayer5120 <kmayer1@cnm.edu>
2019-11-19 17:02:04 -0700

    Fixed fields in Message model

--- MayerP4/Models/Message.cs
+++ MayerP4/Models/Message.cs
@@ -8,18 +8,68 @@ namespace MayerP4.Models
 {
     public class Message
     {
+        private bool isQuota;
+        private bool isGeneralMessage;
+
         public int Id { get; set; }
+
         [Required]
         [StringLength(20, MinimumLength = 5)]
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string LastName { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         public string Subject { get; set; }
-        [Display(Name="Message Body")]
+
+        [Display(Name = "Comment/Quota Request")]
         [Required]
         public string Text { get; set; }
+
+
+        [Display(Name = "Quota")]
+        public bool IsQuota
+        {
+            get { return isQuota; }
+            set
+            {
+                isQuota = value;
+                if (isQuota)
+                {
+                    isGeneralMessage = false;
+                }
+            }
+        }
+
+        [Display(Name = "General Message")]
+        public bool IsGeneralMessage
+        {
+            get { return isGeneralMessage; }
+            set
+            {
+                isGeneralMessage = value;
+                if (isGeneralMessage)
+                {
+                    isQuota = false;
+                }
+            }
+        }
+
+        [Display(Name = "Web app")]
+        public bool IsWebApp { get; set; }
+
+        [Display(Name = "Phone app")]
+        public bool IsPhoneApp { get; set; }
+
+        [Display(Name = "Windows app")]
+        public bool IsWindowsApp { get; set; }
+
     }
 }
 
